f6f821270163fc5e4939e260bd65e855a179293a
liuqiiiiiii <982252163@qq.com>
2018-11-21 11:21:45 +0800

    delete copy file

--- React-official/src/component&props.jsx
+++ React-official/src/component&props.jsx
@@ -104,10 +104,21 @@ function Avatar(props) {
   )
 }
 
-function comment(props) {
+function Comment(props) {
   return (
     <div className="Comment">
-      <Avatar user={props.author} />
+      <div className="UserInfo">
+        <Avatar user={props.author} />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
     </div>
-  )
+  );
 }
